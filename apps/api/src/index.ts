d099d1f0b00474dc1ec4cd7e1aa562dbbebbd1e7
jeremydevv <194394267+jeremydevv@users.noreply.github.com>
2025-10-06 15:47:51 -0400

    Starting Cloudflare Worker API

---
+++ apps/api/src/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "itty-router";
+
+const router = Router();
+
+// services
+import {handleAuth} from "./routes/auth";
+
+router.get("/", () => new Response("Locora API is running 🚀", { status: 200 }));
+
+router.all("/auth/*", handleAuth);
+router.all("*", () => new Response("Not Found", { status: 404 }));
+
+export default {
+    fetch: (req: Request, env: any, ctx: ExecutionContext) => router.handle(req, env, ctx),
+};
--- apps/site/locora/src/components/button.tsx
+++ apps/site/locora/src/components/button.tsx
@@ -3,7 +3,9 @@ import Embossed from "./embossed";
 
 interface buttonProps {
     text?: string;
-    shape: "circle" | "square";
+    type?: "default" | "black" | "white";
+    shape?: "circle" | "square";
+    size?: "small" | "medium" | "large" | "extra-large" | undefined;
     otherProps?: string;
     preChildren?: React.ReactNode;
     children?: React.ReactNode;
@@ -12,22 +14,29 @@ interface buttonProps {
 
 export default function BaseButton({
     text,
-    shape,
+    shape = "square",
     otherProps,
     children,
     preChildren,
+    size = "medium",
+    type = "default",
     hasBackground = true,
 }: buttonProps) {
-    var BaseStyle =
-        "text-white py-2 px-4 rounded-lg drop-shadow-lg font-semibold flex items-center justify-center ";
+    var BaseStyle = "text-white py-2 px-4 rounded-lg drop-shadow-lg font-semibold flex items-center justify-center ";
+
     var SizeAnimations =
         "hover:scale-105 active:scale-97 transition-transform duration-200 ease-in-out style-smooth ";
 
     BaseStyle += SizeAnimations;
 
     if (hasBackground) {
-        BaseStyle +=
-            "bg-gradient-to-b from-bay-of-many-700 to-bay-of-many-400 transition-colors ";
+        if (type === "default") {
+            BaseStyle += "bg-gradient-to-b from-bay-of-many-700 to-bay-of-many-400 transition-colors ";
+        } else if (type === "black") {
+            BaseStyle += "bg-gradient-to-b from-gray-900 to-gray-600 transition-colors ";
+        } else if (type === "white") {
+            BaseStyle += "bg-gradient-to-b from-gray-200 to-gray-50 text-gray-800 transition-colors ";
+        }
         BaseStyle += Embossed();
     }
 
@@ -37,6 +46,16 @@ export default function BaseButton({
 
     if (otherProps) {
         BaseStyle += otherProps;
+    }
+
+    if (size === "small") {
+        BaseStyle += "text-sm ";
+    } else if (size === "medium") {
+        BaseStyle += "text-base ";
+    } else if (size === "large") {
+        BaseStyle += "text-lg ";
+    } else if (size === "extra-large") {
+        BaseStyle += "text-xl ";
     }
 
     return (
