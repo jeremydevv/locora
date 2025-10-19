220c02966fe3ab06b49c126960af8da17d9e4bbb
jeremydevv <194394267+jeremydevv@users.noreply.github.com>
2025-10-19 19:45:04 -0400

    Fixing deployment and Linter

--- apps/site/locora/src/App.tsx
+++ apps/site/locora/src/App.tsx
@@ -1,14 +1,4 @@
 import './App.css'
-
-
-// comps
-import TopBar from './components/topbar';
-import Seacher from './components/seacher';
-
-// assets
-import clouds from "./assets/Clouds3.png"
-import TypeWriter from './effects/Typewriter';
-
 // pages
 import Landing from './pages/landing';
 import Waitlist from './pages/waitlist';
