f37115a5df01f21e3829fd6831d07b82e6ae029d
slimeysnufs <194394267+jeremydevv@users.noreply.github.com>
2025-10-20 10:39:42 -0400

    Starting Waitlist

--- apps/site/locora/src/pages/waitlist.tsx
+++ apps/site/locora/src/pages/waitlist.tsx
@@ -1,7 +1,34 @@
+import BaseInput from "../components/baseinput"
+import Footer from "../components/footer"
+import cloud from "../assets/Clouds3.png"
+
 function WaitlistPage() {
     return (
         <>
-            <h1>Waitlist page</h1>
+
+            <img className="absolute min-w-screen xl:top-25 lg:top-20 md:top-15 sm:top-20" src={cloud} />
+            
+            <div 
+                className="flex min-h-screen bg-bay-of-many-50 items-center justify-center"
+            >
+
+                {/* main container */}
+                <div>
+                    <h1
+                        className="font-black font-black inline-block text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-cyan-500 to-bay-of-many-500 text-7xl"
+                    >
+                        Waitlist
+                    </h1>
+                    <p>
+                        
+                    </p>
+                    <BaseInput placeholder="Email or Phone Number" />
+                </div>
+
+            </div>
+
+            <Footer />
+
         </>
     )
 }
