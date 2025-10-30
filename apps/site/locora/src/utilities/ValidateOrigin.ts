998516bc67830fc8cc3d40110878239b5ea6328b
jeremydevv <194394267+jeremydevv@users.noreply.github.com>
2025-10-29 21:23:40 -0400

    Added CORS to API Proxy

---
+++ apps/site/locora/src/utilities/ValidateOrigin.ts
@@ -0,0 +1,9 @@
+export default function(Origin : string) {
+
+    if (Origin?.startsWith("http://localhost")) return true;
+    if (Origin?.startsWith("https://waitlist-dev.locora.pages.dev")) return true;
+    if (Origin?.startsWith("https://locora.org")) return true;
+    if (Origin?.startsWith("https://api.locora.org")) return true;
+    return false
+    
+}
