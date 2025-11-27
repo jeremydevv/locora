65a12d2f3009a2bd365e233fbeec64915747421b
Jeremy Mathew <194394267+jeremydevv@users.noreply.github.com>
2025-11-27 16:11:13 -0500

    Dynamically changing API request URL

--- apps/app/locora/src/utilities/BaseAPIUrl.ts
+++ apps/app/locora/src/utilities/BaseAPIUrl.ts
@@ -3,7 +3,9 @@ const ENVIRONMENT = import.meta.env.VITE_ENVIORNMENT;
 export default function baseAPIUrl() {
     if (ENVIRONMENT === "dev") {
         return "http://localhost:6767";
-    } else {
+    } else if (ENVIRONMENT == "main") {
         return "https://api.locora.org";
+    } else {
+        return `https://${ENVIRONMENT}-locora-api.jeremymathew100.workers.dev`
     }
 }
