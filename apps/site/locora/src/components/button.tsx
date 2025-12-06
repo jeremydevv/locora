64c8de4cf1a228cc5d3b7a130ab2635e6ac21959
Jeremy Mathew <194394267+jeremydevv@users.noreply.github.com>
2025-12-06 17:35:27 -0500

    Made the text for btn black

--- apps/site/locora/src/components/button.tsx
+++ apps/site/locora/src/components/button.tsx
@@ -36,7 +36,7 @@ export default function BaseButton({
     hasBackground = true,
     onClick = () => {},
 }: buttonProps) {
-    let BaseStyle = "text-white py-2 cursor-pointer px-4 rounded-lg drop-shadow-lg font-semibold flex items-center justify-center ";
+    let BaseStyle = "py-2 cursor-pointer px-4 rounded-lg drop-shadow-lg font-semibold flex items-center justify-center ";
 
     let SizeAnimations =
         "hover:scale-105 active:scale-97 transition-transform duration-10 ease-in-out style-smooth ";
@@ -45,11 +45,11 @@ export default function BaseButton({
 
     if (hasBackground) {
         if (type === "default") {
-            BaseStyle += "bg-gradient-to-b from-blue-700 to-bay-of-many-500 transition-colors ";
+            BaseStyle += "bg-gradient-to-b from-blue-700 text-white to-bay-of-many-500 transition-colors ";
         } else if (type === "black") {
-            BaseStyle += "bg-gradient-to-b from-gray-900 to-gray-600 transition-colors ";
+            BaseStyle += "bg-gradient-to-b from-gray-900 to-gray-600 text-white transition-colors ";
         } else if (type === "white") {
-            BaseStyle += "bg-gradient-to-b from-gray-200 to-gray-200 text-black/80 transition-colors ";
+            BaseStyle += "bg-gradient-to-b from-gray-200 to-gray-200 text-black transition-colors ";
         }
         BaseStyle += Embossed();
     }
