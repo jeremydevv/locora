3dd77c6e1735777ddb3eca2d0bfd2d14d366d2ab
Jeremy Mathew <194394267+jeremydevv@users.noreply.github.com>
2026-01-04 19:49:12 -0500

    Starting Explore Page Struct

--- apps/app/locora/src/pages/Explore.tsx
+++ apps/app/locora/src/pages/Explore.tsx
@@ -1,9 +1,40 @@
+import { useState } from "react"
+
+interface Query {
+    text : string,
+    orderType : "Default" | "Top",
+    storeType : "Food" | "Retail"
+}
+
 export default function Explore() {
+
+    const [currentQuery, setCurrentQuery] = useState<Query>({
+        text : "",
+        orderType : "Default",
+        storeType : "Food",
+    }) 
 
     return (
         <>
-            <div>
+            <div
+                className="w-full min-h-full bg-bay-of-many-500"
+            >
                 
+                {/* featured places (manual)*/}
+                <div>
+
+                </div>
+
+                {/* reccomended (get key words out of search, cache, randomly pick) */}
+                <div>
+
+                </div>
+
+                {/* recently searched places (cached last searched easy) */}
+                <div>
+
+                </div>
+
             </div>
         </>
     )
